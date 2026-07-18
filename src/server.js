db57ffd6a672f19ef56e8ee024d6f45b3010a706
Yash-Sharma-Programer <yashbca029@gmail.com>
2026-07-18 23:56:50 +0530

    Changes in server.js

--- src/server.js
+++ src/server.js
@@ -2,12 +2,15 @@ import { connectDatabase } from './config/db.js';
 import { env } from './config/env.js';
 import { createApp } from './app.js';
 import { attachRealtimeServer } from './config/realtime.js';
-import { createDeadlineNotifications } from './jobs/deadline.job.js';
 
 await connectDatabase();
+
 const app = createApp();
-const server = app.listen(env.port, () =>
-  console.log(`TaskFlow API listening on http://localhost:${env.port}`),
-);
+
+const server = app.listen(env.port, () => {
+  console.log(
+    `TaskFlow API listening on port ${env.port}`,
+  );
+});
+
 attachRealtimeServer(app, server);
-setInterval(() => createDeadlineNotifications().catch(console.error), 60 * 60 * 1000).unref();
